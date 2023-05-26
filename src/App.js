22a58c9c8f7c293c7ff50a26168828e241291716
Monica Gerard <mlgvla@gmail.com>
2023-05-25 21:33:41 -0400

    sets up explicit Fragment example

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import "./styles.css";
+import "./styles.css"
+
+
+const posts = [
+   { id: 1, title: "An update", body: "It's been a while since I posted..." },
+   { id: 2, title: "My new blog", body: "I am starting a new blog!" },
+]
 
 export default function App() {
-  return (
-    <div className="App">
-      <Blog />
-    </div>
-  );
+   return (
+      <div className="App">
+         <Blog />
+      </div>
+   )
 }
 
 function Blog() {
-  return (
-    <>
-      <Post title="An update" body="It's been a while since I posted..." />
-      <Post title="My new blog" body="I am starting a new blog!" />
-    </>
-  );
-}
-
-function Post({ title, body }) {
-  return (
-    <>
-      <PostTitle title={title} />
-      <PostBody body={body} />
-      <hr></hr>
-    </>
-  );
+   return posts.map((post) => (
+      <>
+         <PostTitle title={post.title} />
+         <PostBody body={post.body} />
+      </>
+   ))
 }
 
 function PostTitle({ title }) {
-  return <h1>{title}</h1>;
+   return <h1>{title}</h1>
 }
 
 function PostBody({ body }) {
-  return (
-    <article>
-      <p>{body}</p>
-    </article>
-  );
+   return (
+      <article>
+         <p>{body}</p>
+      </article>
+   )
 }
